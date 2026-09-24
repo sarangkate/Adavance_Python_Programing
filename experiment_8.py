3d67bcd95d8e45131824de15839b20a7073da3af
Sarang Kate <sarangkate2@gmail.com>
2026-09-24 11:39:04 +0530

    Update experiment_8.py

--- experiment_8.py
+++ experiment_8.py
@@ -1,15 +1,27 @@
-file = open("input.txt", "r")
+# Input file
+file = open("Input.txt","w")
+file.write("Name: Sarang Kate")
+file.write("\nDiv: SY-1")
+file.write("\nBatch: B")
+file.write("\nDepartment: CSE")
+
+file.close()
+
+# Counting no of rows
+file = open("Input.txt","r")
 lines = file.readlines()
-count = len(lines)
+count = 0
+for line in lines:
+    count += 1
+print("No of lines in file: ",count)
 
 first_two_lines = lines[:2]
 file.close()
 
-print("Total number of lines:", count)
-file1 = open("output.txt", "w")
+#Writing 1st 2 lines in Output file
+file2 = open("Output.txt","w")
 for line in first_two_lines:
-    file1.write(line)
+    file2.write(line)
+file2.close()
 
-file1.close()
-
-print("First two lines have been written to output.txt")
+print("2 lines extarcted to Output file from Input file")
